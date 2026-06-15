162c9db7c8e691959c29330dac08781fa695e9e2
Stiven <visualstudio2732@gmail.com>
2026-06-15 17:00:02 -0500

    Agregue las tabals de multiplicar

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ class Notes:
         self.__content = content
 
 
-    def get_title(self):
+    def get_title(self) :
         return self.__title
     def get_content(self):
         return self.__content
 Note1 = Notes("Note 1","This is the content of note 1") 
 
 print(Note1.get_title())
-print(Note1.get_content())
+print(Note1.get_content())  
+
+
+numero = int(input("Enter a number: "))
+for i in range (11):
+    print (f"{numero} x {i} = {numero * i}")
