bfc77172408a8149638a2d8058f466facdba9e74
Stiven <visualstudio2732@gmail.com>
2026-06-16 15:22:11 -0500

    Today i did a claculate only can calculate sum with decimal or int

---
+++ calculate.py
@@ -0,0 +1,11 @@
+numero1 = float(input("Enter the first number: "))
+numero2 = float(input("Enter the second number: "))
+def sumar(numero1,numero2):
+    resultado = numero1 + numero2
+    print(f"The sum is {resultado}")
+    return resultado
+    
+sumar(numero1,numero2)
+
+
+
